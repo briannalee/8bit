d11922b0f67cd4d2670616d4aec3d4adc4bf3bc5
Brianna Ragsdale <mail@brianna.cc>
2023-03-03 20:18:31 -0600

    bit size & window size adjustable, visual overflow

--- src/VisualBit.ts
+++ src/VisualBit.ts
@@ -1,4 +1,6 @@
-export class Circle {
+import { Environment } from "./Environment";
+
+export class VisualBit {
     /**
      * Class that holds info about a circle drawn on an HTML canvas, representing a visual bit of data.
      */
@@ -11,27 +13,29 @@ export class Circle {
     top: number;
     right: number;
     bottom: number;
-    context: CanvasRenderingContext2D;
+    env: Environment;
+    fillColor: string;
 
     /**
      * 
      * @param x Centerpoint X coordinate to draw circle
      * @param y Centerpoint Y coordinate to draw circle
      * @param radius Radius of circle to draw
-     * @param context Canvas context to draw on
+     * @param env Canvas context to draw on
      */
-    constructor(x : number, y: number, radius:number, context: CanvasRenderingContext2D)
-    constructor(x : number, y: number, radius:number, context: CanvasRenderingContext2D, set?:Boolean) {
+    constructor(x : number, y: number, radius:number, env: Environment)
+    constructor(x : number, y: number, radius:number, env: Environment, set?:Boolean) {
         this.x = x;
         this.y = y;
         this.set = set ?? false;
         this.radius = radius;
-        this.left = x-radius;
-        this.top = y-radius;
-        this.right = x+radius;
-        this.bottom = y+radius;
-        this.context = context;
-        this.draw(this.context);
+        this.left = this.x-radius-1;
+        this.top = this.y-radius-1;
+        this.right = this.x+radius+1;
+        this.bottom = this.y+radius+1;
+        this.env = env;
+        this.fillColor = "white";
+        this.draw();
     }
 
     /**
@@ -39,7 +43,8 @@ export class Circle {
      */
     switchBit() {
         this.set = !this.set;
-        this.draw(this.context);
+        this.draw();
+        
     }
 
     /**
@@ -48,19 +53,27 @@ export class Circle {
      */
     setBit(bit: boolean) {
         this.set = bit;
-        this.draw(this.context);
+        this.draw();
+    }
+
+    setOverflow(overflow: boolean) {
+        if(overflow) this.fillColor = "red"; 
+        else this.fillColor = "white";
     }
 
     /**
      * Draws the circle
      * @param context - The canvas context to draw on
      */
-    draw(context: CanvasRenderingContext2D): void {
-        context.clearRect(this.left-2, this.top-2, this.radius*2+4, this.radius*2+4);
+    draw(): void {
+        const context: CanvasRenderingContext2D = this.env.canvas.context;
+        context.clearRect(this.left, this.top, this.radius*2+2, this.radius*2+2);
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        context.fillStyle = this.set ? "white" : "#0f0f0f";
+        context.fillStyle = this.set ? this.fillColor : "#0f0f0f";
+        context.strokeStyle = this.set ? "#95c299" : "#330400" ;
         context.fill();
+        context.stroke();
         context.closePath();
     }
 }
